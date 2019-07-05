f2440b1252950bf5267ab63e691adb266e52d9e3
DenisCalixto <deniscalixto@gmail.com>
2019-07-05 00:00:15 -0700

    Login page with route and change to open it from navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingComponent} from './components/landing/landing.component';
 import { InfoComponent} from './components/info/info.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 const routes: Routes = [
   {path: '', component: LandingComponent},
   {path: 'info', component: InfoComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent},
+  {path: 'login', component: AuthComponent}
 ];
 
 @NgModule({
