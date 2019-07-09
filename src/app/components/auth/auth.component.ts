0b4fc29d3be840c98fa4a34b930a8b7f78bdcaed
DenisCalixto <deniscalixto@gmail.com>
2019-07-08 22:26:08 -0700

    navigating to landing page after logging in

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { AuthService, AuthResponseData } from './auth.service';
 
@@ -14,7 +15,7 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -41,6 +42,7 @@ export class AuthComponent {
       resData => {
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(['']);
       },
       errorMessage => {
         console.log(errorMessage);
