fc3ada3955be473e2e5320b678f6ec116bd63b65
DenisCalixto <deniscalixto@gmail.com>
2019-07-10 16:17:19 -0700

    Fetching the total packaging of a user

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,29 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { TotalPackaging } from './total-packaging.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+
+  totalPackaging;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Send Http request
+    this.fetchTotalPackaging(10);
+  }
+
+  private fetchTotalPackaging(idPerson) {
+    this.http
+      .get<{[key: string]: TotalPackaging }>('http://greenfill.deniscalixto.wmdd.ca/person/' + idPerson + '/totalpackaging')
+      .subscribe(totalItem => {
+        this.totalPackaging = totalItem;
+      });
+  }}
