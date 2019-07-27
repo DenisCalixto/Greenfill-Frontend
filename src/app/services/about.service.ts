67e8ef94b96b8ca83a5eaebe8a8c014cb81c59d8
andreluizfalco <48778069+andreluizfalco@users.noreply.github.com>
2019-07-26 19:15:08 -0700

    profile

--- src/app/services/about.service.ts
+++ src/app/services/about.service.ts
@@ -12,37 +12,37 @@ export class AboutService {
         name: 'Andre Falco',
         role: 'Project Manager, Designer',
         info: 'Brief description of background, role, responsibilities and so on. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-        image: '../../assets/andre.jpg'
+        image: '../../assets/andre.png'
       },
       {
         name: 'Denis Calixto',
         role: 'Lead Developer',
         info: 'Brief description of background, role, responsibilities and so on. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-        image: '../../assets/denis.jpg'
+        image: '../../assets/denis.png'
       },
       {
         name: 'Marinela Poso',
         role: 'Lead Designer, UX',
         info: 'Brief description of background, role, responsibilities and so on. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-        image: '../../assets/marinela.jpg'
+        image: '../../assets/marinela.png'
       },
       {
         name: 'June Jung',
         role: 'Front-end Developer, UX Designer',
         info: 'Brief description of background, role, responsibilities and so on. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-        image: '../../assets/june.jpg'
+        image: '../../assets/june.png'
       },
       {
         name: 'Brian Nguyen',
         role: 'Back-end Developer',
         info: 'Brief description of background, role, responsibilities and so on. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-        image: '../../assets/brian.jpg'
+        image: '../../assets/brian.png'
       },
       {
         name: 'Thanh Nguyen',
         role: 'UX/UI Designer',
         info: 'Brief description of background, role, responsibilities and so on. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-        image: '../../assets/thanh.jpg'
+        image: '../../assets/thanh.png'
       }
     ]
   }
